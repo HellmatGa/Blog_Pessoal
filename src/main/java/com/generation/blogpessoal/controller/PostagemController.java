52a2cf56d59c334fa084c94410ead5b6a0483ab3
Gabriel Arruda <hellmatgabriel@icloud.com>
2024-05-22 12:26:46 -0300

    atualizada postagens, cadastros e registros

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,26 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController // anotação que diz para string que essa é uma controladora de rotas e acesso aos metodos
 @RequestMapping("/postagens") // rota para chegar nessa classe "insomnia"
@@ -25,6 +35,48 @@ public class PostagemController {
 		//reponseEntity - Classe 
 		return ResponseEntity.ok(postagemRepository.findAll());
 		//SELCET * FROM tb_postagens
+	}
+
+	//localhost:8080/postagens/2
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		//findById = SELECT * FROM tb_postagens WHERE id = 1;
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	//SELECT * FROM tb_postagens WHERE titulo = "titulo";
+	@GetMapping("/titulo/{titulo}")//localhost:8080/postagens/titulo/Postagem 02
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", "2024-12-31 14:05:01");
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		//retorno em formato ResponseEntity
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta-> ResponseEntity.status(HttpStatus.OK)
+				.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);
 	}
 	
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,16 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 //JpaRepository - classe JPA - metodos que vão realizar query no banco
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 	// SELECT * FROM tb_postagens
+	
+	//SELCET * FROM tb_postagens WHERE titulo
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
